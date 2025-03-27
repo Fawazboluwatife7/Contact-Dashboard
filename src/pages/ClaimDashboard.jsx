f02018a1149f88da5772ab9bbb72f006cbee4cc4
Fawazboluwatife7 <147808555+Fawazboluwatife7@users.noreply.github.com>
2025-03-27 04:41:07 +0100

    first commit

--- src/pages/ClaimDashboard.jsx
+++ src/pages/ClaimDashboard.jsx
@@ -1,156 +1,318 @@
-import React, { useEffect, useState } from "react";
-import { MdFilterAlt } from "react-icons/md";
-import DashboardCarousels from "./DashboardCarousels";
+import React, { useState, useEffect } from "react";
 
-function DateDropdown({ options, sendNumber, className }) {
-    const [dateType, setDateType] = useState("1");
+import { useNavigate } from "react-router-dom";
+import { CiCalendar } from "react-icons/ci";
 
-    const pickDate = (event) => {
-        const number = event.target.value;
-        setDateType(number);
-        console.log("Number selected:", number);
-        sendNumber(number);
+const ClaimDashboard = () => {
+    const navigate = useNavigate();
+    const handleNavigate = (path) => {
+        navigate(path);
     };
+    const user = JSON.parse(localStorage.getItem("user"));
+    const [data, setData] = useState([]);
+    const [declined, setDeclinedPA] = useState([]);
+    const [
+        authorizationApprovedClaimPending,
+        setAuthorizationApprovedClaimPending,
+    ] = useState([]);
+    const [AuthorizationPending, setAuthorizationPending] = useState([]);
+    const [AuthorisationPending, setAuthorisationPending] = useState([]);
+    const [openPAOne, setOpenPAOne] = useState([]);
+    const [openPATwo, setOpenPATwo] = useState([]);
+    const combinedOpenPA = [...openPAOne, ...openPATwo];
+    const [totalPA, TotalPA] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [showAll, setShowAll] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [todayDate, setTodayDate] = useState("");
+    const [dailyPA, setDailyPA] = useState("");
+    const [date, setTodaysDate] = useState("");
+    const apiUrl = import.meta.env.VITE_API_BASE_URL;
+    const [currentDate, setCurrentDate] = useState("");
+
+    const [pendingPA, setPendingPA] = useState([]);
+    const [peopleWaiting, setPeopleWaiting] = useState("");
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 10;
+
+    const totalPages = Math.ceil(pendingPA.length / itemsPerPage);
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    const paginatedResults = pendingPA.slice(startIndex, endIndex);
+
+    function formatISOToCustom(dateString) {
+        if (!dateString) return ""; // Handle cases where DateIssued might be undefined/null
+
+        const date = new Date(dateString);
+
+        // Extract components
+        const day = String(date.getDate()).padStart(2, "0");
+        const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
+        const year = date.getFullYear();
+        let hours = date.getHours();
+        const minutes = String(date.getMinutes()).padStart(2, "0");
+
+        // Convert to 12-hour format with AM/PM
+        const ampm = hours >= 12 ? "PM" : "AM";
+        hours = hours % 12 || 12; // Convert 0 to 12 for midnight
+
+        return `${month}/${day}/${year} ${hours}:${minutes} ${ampm}`;
+    }
+
+    console.log(
+        "paaa",
+        fetch(
+            `${apiUrl}api/EnrolleeProfile/GetEnrolleePreauthorizations?Fromdate=${currentDate}&Todate=${currentDate}&cifno=0&PAStatus&visitid`,
+            {
+                method: "GET",
+            },
+        ),
+    );
+
+    useEffect(() => {
+        const today = new Date().toISOString().split("T")[0]; // Gets YYYY-MM-DD
+        setCurrentDate(today);
+
+        console.log("date", today);
+    }, []);
+
+    useEffect(() => {
+        if (currentDate) {
+            getDailyPA();
+
+            const interval = setInterval(() => {
+                getDailyPA();
+            }, 300000);
+
+            return () => clearInterval(interval);
+        }
+    }, [currentDate]);
+
+    async function getDailyPA() {
+        setLoading(true);
+        try {
+            const response = await fetch(
+                `${apiUrl}api/EnrolleeProfile/GetEnrolleePreauthorizations?Fromdate=${currentDate}&Todate=${currentDate}&cifno=0&PAStatus&visitid`,
+                {
+                    method: "GET",
+                },
+            );
+
+            const data = await response.json();
+            console.log("xp", data.result);
+
+            TotalPA(data.result.length);
+
+            if (data.status === 200) {
+                const validStatuses = [
+                    "Authorization Pending",
+                    "Authorisation pending",
+                    "Authorisation approved claim pending",
+                    "Declined",
+                ];
+
+                const filteredData = data.result.filter((item) =>
+                    validStatuses.includes(item.PAStatus),
+                );
+
+                const PendingPA = filteredData.filter(
+                    (item) =>
+                        item.PAStatus.toLowerCase() === "authorization pending",
+                );
+
+                const uniqueMemberNumbers = new Set(
+                    PendingPA.map((item) => item.MembernUmber),
+                );
+
+                const uniqueMemberCount = uniqueMemberNumbers.size;
+
+                console.log("count", uniqueMemberCount);
+
+                const PendingPATwo = filteredData.filter(
+                    (item) =>
+                        item.PAStatus.toLowerCase() === "authorisation pending",
+                );
+
+                const uniqueMember = new Set(
+                    PendingPATwo.map((item) => item.MembernUmber),
+                );
+
+                const uniqueMembercc = uniqueMember.size;
+
+                console.log("counttwo", uniqueMembercc);
+
+                setPeopleWaiting(uniqueMembercc + uniqueMemberCount);
+
+                const ClaimPending = filteredData.filter(
+                    (item) =>
+                        item.PAStatus.toLowerCase() ===
+                        "authorisation approved claim pending",
+                );
+                const DeclinedPA = filteredData.filter(
+                    (item) => item.PAStatus.toLowerCase() === "declined",
+                );
+
+                const allPendingPa = [...PendingPA, ...PendingPATwo];
+                setPendingPA(allPendingPa);
+                setOpenPAOne(PendingPA);
+                setOpenPATwo(PendingPATwo);
+
+                setAuthorizationPending(PendingPA.length);
+                setAuthorisationPending(PendingPATwo.length);
+                setAuthorizationApprovedClaimPending(ClaimPending.length);
+                setDeclinedPA(DeclinedPA.length);
+
+                setAdjudicated(filteredData.length); // Fix: adjudicatedItems wasn't defined
+            } else {
+                console.error(
+                    "Failed to fetch data or unexpected response format.",
+                );
+            }
+        } catch (error) {
+            console.error("get pa:", error);
+            setDailyPA([]);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        const today = new Date().toISOString().split("T")[0]; // Gets YYYY-MM-DD
+        setCurrentDate(today);
+    }, []);
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const handleSeeAll = () => {
+        navigate("/ticket", { state: { filter: "Escalation" } });
+    };
+
+    // Dummy data for Escalations
+    const escalations = [
+        {
+            description: "Escalation 1: Issue with the server not responding.",
+        },
+        {
+            description:
+                "Escalation 2: User unable to access their account due to login error.",
+        },
+        {
+            description:
+                "Escalation 3: Critical bug in the production environment affecting users.",
+        },
+    ];
+
+    if (error) {
+        return (
+            <div className="w-full p-5 bg-lightblue mt-10">
+                <p className="text-red-500">{error}</p>
+            </div>
+        );
+    }
+
+    const paginateData = showAll ? data : data.slice(0, 7);
+
+    function getTodayDate() {
+        const today = new Date();
+        const day = today.getDate().toString().padStart(2, "0"); // Ensure two-digit format
+        const month = today.toLocaleString("en-US", { month: "short" }); // Get three-letter month
+        const year = today.getFullYear();
+
+        setTodaysDate(today);
+
+        return `${day}-${month}-${year}`;
+    }
+
+    useEffect(() => {
+        setTodayDate(getTodayDate());
+    }, []);
 
     return (
         <div>
-            <select value={dateType} onChange={pickDate} className={className}>
-                <option value="" disabled>
-                    Treatment date
-                </option>
-                {options.map((option) => (
-                    <option key={option.value} value={option.value}>
-                        {option.label}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
-}
+            <div className="bg-lightblue w-[100%] ml-auto h-[100vh]">
+                <div className=" px-4">
+                    <div className="px-2">
+                        <div className="flex justify-between items-center w-full pt-5">
+                            {/* Logo on the left */}
+                            <img
+                                src="leadway_health_logo-dashboard.png"
+                                alt="Logo"
+                                className="w-32"
+                            />
 
-const ClaimDashboard = () => {
-    const apiUrl = import.meta.env.VITE_API_BASE_URL;
-    const [open, setOpen] = useState("");
+                            {/* Calendar box on the right */}
+                            <div className="bg-white rounded-md w-[10rem] h-[3rem] py-2 flex items-center gap-2 px-3">
+                                <CiCalendar className="text-[20px]" />
+                                <h1 className="font-medium">{todayDate}</h1>
+                            </div>
+                        </div>
 
-    const [items, setItems] = useState([]);
-    const [filteredClaimsItems, setClaimsItems] = useState([]);
-    const [totalApiData, setTotalApiData] = useState([]);
-    const [totalItems, setTotalItems] = useState(0);
-    const [startDate, setStartDate] = useState("2024-11-12");
-    const [endDate, setEndDate] = useState("2024-12-29");
-    const [dateType, setDateType] = useState("1");
-    const [isLoading, setIsLoading] = useState(false);
-    const [tableData, setTableData] = useState([]);
-    const [totalUnpaidCLaims, setUnpaidCLaims] = useState("");
-    const [uniqueProvidersAboveFiveDays, setUniqueProvidersAboveFiveDays] =
-        useState("");
-    const [totalUnpaidClaimValue, setTotalUnpaidClaimValue] = useState("");
-    const [claimAboveFiveDaysDuration, setClaimAboveFiveDaysDuration] =
-        useState("");
-    const [totalClaimValueAboveFiveDays, setTotalClaimValueAboveFiveDays] =
-        useState("");
+                        <h4 className="mt-2 mb-2 text-[20px] text-red-500 font-bold">
+                            Call centre dashboard{" "}
+                        </h4>
+                    </div>
 
-    const dateTypes = [
-        { label: "TreatmentDate", value: 1 },
-        { label: "Finance on", value: 2 },
-        { label: "Encounter month to", value: 3 },
-        { label: "Batch received on", value: 6 },
-    ];
-
-    const [claimsOperationsTAT, setClaimsOperationsTAT] = useState(0);
-    const [internalControlTAT, setInternalControlTAT] = useState(0);
-    const [financeTAT, setFinanceTAT] = useState(0);
-    const [turnAroundTAT, setTurnAroundTAT] = useState(0);
-
-    useEffect(() => {
-        getDashboardData();
-
-        const today = new Date();
-        const end = today.toISOString().split("T")[0];
-
-        const pastDate = new Date();
-        pastDate.setDate(today.getDate() - 30);
-        const start = pastDate.toISOString().split("T")[0];
-
-        setStartDate(start);
-        setEndDate(end);
-
-        getBatchTotalOfTheLastThirtyDays();
-    }, []);
-
-    async function getDashboardData() {
-        const response = await fetch(
-            `${apiUrl}/api/EnrolleeClaims/GetBatchSumaary?Fromdate=${startDate}&Todate=${endDate}&DateType=${dateType}`,
-            {
-                method: "GET",
-            },
-        );
-
-        const data = await response.json();
-
-        if (data.status === 200) {
-            const validStatuses = [
-                "Awaiting Adjudication",
-                "Open                ",
-                "Quality Assurance",
-                "Adjudicated",
-            ];
-
-            const filteredData = data.result.filter((item) =>
-                validStatuses.includes(item.BatchStatus),
-            );
-            setClaimsItems(filteredData);
-        }
-        setTotalApiData(data);
-    }
-
-    const totalUnits = filteredClaimsItems.reduce(
-        (sum, item) => sum + (item.Units || 0),
-        0,
-    );
-
-    const totalBatchTotal = filteredClaimsItems.reduce(
-        (sum, item) => sum + (item.BatchTotal || 0),
-        0,
-    );
-
-    const formattedTotal = totalBatchTotal.toLocaleString("en-US", {
-        minimumFractionDigits: 1,
-        maximumFractionDigits: 2,
-    });
-    const unitsTotal = totalUnits.toLocaleString("en-US", {
-        minimumFractionDigits: 1,
-        maximumFractionDigits: 2,
-    });
-
-    // const filteredItems = filteredClaimsItems.reduce(
-    //     (sum, item) =>
-    //         item.DateDiffWithoutWeekend > 5 ? sum + (item.Units || 0) : sum,
-    //     0,
-    // );
-
-    // const formattedfilteredItems = filteredItems.toLocaleString("en-US");
-
-    // setClaimAboveFiveDaysDuration(formattedfilteredItems);
-
-    async function getBatchTotalOfTheLastThirtyDays() {
-        const response = await fetch(
-            `${apiUrl}/api/EnrolleeClaims/GetBatchSumaary?Fromdate=${startDate}&Todate=${endDate}&DateType=2`,
-            {
-                method: "GET",
-            },
-        );
-        const data = await response.json();
-
-        const paidItemsCount = data.result.filter(
-            (item) => item.BatchStatus === "Paid",
-        ).length;
-
-        console.log(`Total paid items: ${paidItemsCount}`);
-    }
-    return (
-        <div className=" w-full h-[100vh]">
-            <DashboardCarousels />
+                    <div className="flex w-full  overflow-y-visible ">
+                        <div className=" w-full  flex gap-2 mr-1 ">
+                            <div className=" flex-1 bg-bl bg-[#5f5f8c84] w-full h-[17.5rem] rounded-md py-4 px-4">
+                                <p className="text-white  text-[45px] pt-2">
+                                    {" "}
+                                    Total PA Request
+                                </p>
+                                <h1 className="text-[80px] font-bold pt-[2.5rem]">
+                                    {totalPA}
+                                </h1>
+                            </div>
+                            <div className=" flex-1 bg-bl bg-[#5f5f8c84] w-full h-[17.5rem] rounded-md py-4 px-4">
+                                <p className="text-white text-[35px]">
+                                    Open Tickets/ number of enrolles
+                                </p>
+                                <h1 className="text-[80px] font-bold ">
+                                    {AuthorisationPending +
+                                        AuthorizationPending}{" "}
+                                    {""} {"/"} {peopleWaiting}
+                                </h1>
+                            </div>
+                            {/* <div className=" flex-1 bg-bl bg-[#5f5f8c84] w-full h-[17.5rem] rounded-md py-4 px-4">
+                                <p className="text-white text-[35px]">
+                                    Open Tickets/ number of enrolles
+                                </p>
+                                <h1 className="text-[80px] font-bold ">
+                                    {AuthorisationPending +
+                                        AuthorizationPending}{" "}
+                                    {""} {"/"} {peopleWaiting}
+                                </h1>
+                            </div> */}
+                        </div>
+                    </div>
+                    <div className=" w-full flex gap-2 mt-4">
+                        <div className="w-full  flex gap-2 mr-1 ">
+                            <div
+                                className="
+                   flex-1 bg-bl  bg-[#5f5f8c84] w-full h-[18.5rem] rounded-md py-4 px-4"
+                            >
+                                <p className="text-white text-[55px]">
+                                    Approved PA Requests
+                                </p>
+                                <span className="text-[100px] font-bold">
+                                    {authorizationApprovedClaimPending}
+                                </span>
+                            </div>
+                            <div className="flex-1 bg-bl bg-[#5f5f8c84] w-full h-[18.5rem] rounded-md py-4 px-4">
+                                <p className="text-white text-[55px]">
+                                    Declined PA
+                                </p>
+                                <span className="text-[100px] font-bold">
+                                    {declined}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
